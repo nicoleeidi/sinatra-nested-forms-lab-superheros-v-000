ba0f86b8e35454d8159c7729be324a250a38b7e7
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-16 20:53:56 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
+get '/' do
+  erb :super_hero
+end
+post '/team' do
 
-
+  erb :team
+end
 end
